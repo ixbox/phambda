edfcaf9fcb5a674771158a22ca0cc1feb701cbe1
Kubo Seiya <s.kubo@i3design.co.jp>
2025-03-13 09:36:43 +0900

    refactor: optimize timezone handling in AbstractLogger
    - Use ternary operator for timezone determination
    - Add tests for environment variable timezone
    - Add test for timezone configuration priority
    - Update documentation to reflect completed timezone features

--- src/Logger/AbstractLogger.php
+++ src/Logger/AbstractLogger.php
@@ -2,6 +2,8 @@
 
 namespace Phambda\Logger;
 
+use DateTimeImmutable;
+use DateTimeZone;
 use Psr\Log\LoggerInterface;
 use Psr\Log\LoggerTrait;
 use Psr\Log\LogLevel;
@@ -12,20 +14,40 @@ abstract class AbstractLogger implements LoggerInterface
     use LoggerTrait;
 
     private string $minimumLevel;
+    private DateTimeZone $timezone;
+    private string $dateFormat;
 
     public function __construct(
         private readonly LogFormatterInterface $formatter,
         string $minimumLevel = LogLevel::DEBUG,
-        private readonly array $defaultContext = []
+        private readonly array $defaultContext = [],
+        private readonly ?LoggerConfiguration $config = null
     ) {
         $this->minimumLevel = $minimumLevel;
+        $this->timezone = $this->determineTimezone();
+        $this->dateFormat = $this->config?->getDateFormat() ?? DateTimeImmutable::RFC3339_EXTENDED;
+    }
+
+    private function determineTimezone(): DateTimeZone
+    {
+        if ($this->config) {
+            return $this->config->getTimezone();
+        }
+
+        return new DateTimeZone(getenv('LOG_TIMEZONE') ?: 'UTC');
+    }
+
+    private function getFormattedTime(): string
+    {
+        $dateTime = new DateTimeImmutable('now', new DateTimeZone('UTC'));
+        return $dateTime->setTimezone($this->timezone)->format($this->dateFormat);
     }
 
     public function log($level, string|Stringable $message, array $context = []): void
     {
         if ($this->shouldLog($level)) {
             $data = [
-                'time' => date('c'),
+                'time' => $this->getFormattedTime(),
                 'level' => $level,
                 'message' => $message,
                 'context' => array_merge($this->defaultContext, $context)
--- tests/Unit/Logger/AbstractLoggerTest.php
+++ tests/Unit/Logger/AbstractLoggerTest.php
@@ -2,10 +2,15 @@
 
 use Phambda\Logger\AbstractLogger;
 use Phambda\Logger\LogFormatterInterface;
+use Phambda\Logger\LoggerConfiguration;
 use Psr\Log\LogLevel;
 
+beforeEach(function () {
+    putenv('LOG_TIMEZONE');
+});
+
 it('filters log levels correctly', function () {
-    $formatter = Mockery::mock(\Phambda\Logger\LogFormatterInterface::class);
+    $formatter = Mockery::mock(LogFormatterInterface::class);
     $formatter->shouldReceive('format')
         ->once()
         ->with(Mockery::on(function ($data) {
@@ -22,4 +27,86 @@ it('filters log levels correctly', function () {
 
     $logger->debug('This is a debug message');
     $logger->info('This is an info message');
+});
+
+it('uses environment timezone when no configuration is provided', function () {
+    putenv('LOG_TIMEZONE=Asia/Tokyo');
+
+    $formatter = Mockery::mock(LogFormatterInterface::class);
+    $formatter->shouldReceive('format')
+        ->once()
+        ->with(Mockery::on(function ($data) {
+            return strpos($data['time'], '+09:00') !== false;
+        }))
+        ->andReturn('');
+
+    $logger = new class($formatter, LogLevel::DEBUG) extends AbstractLogger {
+        public function __construct($formatter, $minimumLevel)
+        {
+            parent::__construct($formatter, $minimumLevel);
+        }
+    };
+
+    $logger->info('Test message');
+});
+
+it('prioritizes configuration timezone over environment variable', function () {
+    putenv('LOG_TIMEZONE=America/New_York');  // UTC-4
+
+    $formatter = Mockery::mock(LogFormatterInterface::class);
+    $formatter->shouldReceive('format')
+        ->once()
+        ->with(Mockery::on(function ($data) {
+            return strpos($data['time'], '+09:00') !== false;  // Asia/Tokyo
+        }))
+        ->andReturn('');
+
+    $config = new LoggerConfiguration('Asia/Tokyo');
+    $logger = new class($formatter, LogLevel::DEBUG, [], $config) extends AbstractLogger {
+        public function __construct($formatter, $minimumLevel, $defaultContext, $config)
+        {
+            parent::__construct($formatter, $minimumLevel, $defaultContext, $config);
+        }
+    };
+
+    $logger->info('Test message');
+});
+
+it('uses UTC timezone when no configuration or environment variable is provided', function () {
+    $formatter = Mockery::mock(LogFormatterInterface::class);
+    $formatter->shouldReceive('format')
+        ->once()
+        ->with(Mockery::on(function ($data) {
+            return strpos($data['time'], '+00:00') !== false;
+        }))
+        ->andReturn('');
+
+    $logger = new class($formatter, LogLevel::DEBUG) extends AbstractLogger {
+        public function __construct($formatter, $minimumLevel)
+        {
+            parent::__construct($formatter, $minimumLevel);
+        }
+    };
+
+    $logger->info('Test message');
+});
+
+it('uses correct timezone from configuration', function () {
+    $formatter = Mockery::mock(LogFormatterInterface::class);
+    $formatter->shouldReceive('format')
+        ->once()
+        ->with(Mockery::on(function ($data) {
+            return strpos($data['time'], '+09:00') !== false;
+        }))
+        ->andReturn('');
+
+    $config = new LoggerConfiguration('Asia/Tokyo');
+    $logger = new class($formatter, LogLevel::DEBUG, [], $config) extends AbstractLogger {
+        public function __construct($formatter, $minimumLevel, $defaultContext, $config)
+        {
+            parent::__construct($formatter, $minimumLevel, $defaultContext, $config);
+        }
+    };
+
+    $logger->info('Test message');
 });
